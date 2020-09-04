f46ce583ff4e055c645e8ab9a0fd905d9915b315
Oscar R <ktaoscar@gmail.com>
2020-09-04 16:15:31 -0600

    Commit Loudmod

---
+++ backend/data.js
@@ -0,0 +1,36 @@
+export default {
+    products:[{
+        _id:'1',
+        name: 'Slim Shady',
+        category: 'Playeras',
+        image: '/images/d1.jpg',
+        price:60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+        countInStock: 3
+    },
+    {
+        _id:'2',
+        name: 'Chicka chicka',
+        category: 'Playeras',
+        image: '/images/d1.jpg',
+        price:50,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+        countInStock: 2
+    },
+    {
+        _id:'3',
+        name: 'Jordan',
+        category: 'Playeras',
+        image: '/images/d1.jpg',
+        price:630,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+        countInStock: 3
+    }
+]
+}
